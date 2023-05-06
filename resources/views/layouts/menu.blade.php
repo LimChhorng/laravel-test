14c0ee59db668e208a5a5bf9c3820b6c6fe17880
LimChhorng <85859433+LimChhorng@users.noreply.github.com>
2023-05-06 12:39:01 +0700

    adminLTE setup

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,12 @@
+<!-- need to remove -->
+<li class="nav-item">
+    <a href="{{ route('home') }}" class="nav-link active">
+        <i class="fa-solid  fas fa-home"></i>
+        <p calss="text-red-500">home</p>
+    </a>
+    <a href="{{ route('home') }}" class="nav-link active">
+        <i class="fa-solid fa-school"></i>
+        <p calss="text-red-500">school</p>
+    </a>
+</li>
+
